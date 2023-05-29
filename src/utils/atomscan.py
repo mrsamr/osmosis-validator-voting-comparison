22aa8a00f4a9169b5ae0b232faa9fb42b7a36901
mrsamr <rmas.eth11@gmail.com>
2023-05-29 17:57:31 +0800

    Added back voting_power field for sorting

--- src/utils/atomscan.py
+++ src/utils/atomscan.py
@@ -14,14 +14,19 @@ def get_validators() -> list:
     r = requests.get(URL, params={'pagination.limit':PAGE_SIZE})
     validators = r.json()['validators']
     validators = [{'address':val['operator_address'],
-                   'name':val['description']['moniker']}
+                   'name':val['description']['moniker'],
+                   'voting_power':float(val['delegator_shares'])}
                   for val in validators]
     
     duplicate_names = [x[0] for x in Counter([x['name'] for x in validators]).items() if x[1] > 1]
     
     validators = [{'address':val['address'],
                    'name':val['name'] if val['name'] not in duplicate_names
-                                      else f"{val['name']} (...{val['address'][-6:]})"}
+                                      else f"{val['name']} (...{val['address'][-6:]})",
+                   'voting_power':val['voting_power']}
                   for val in validators]
+    
+    # Sort by descending voting power
+    validators = sorted(validators, key=lambda x: x['voting_power'], reverse=True)
 
     return validators
